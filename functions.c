a474dca65b3d9f05abd4313c081c745fb1713350
Anewere <94133875+Anewere@users.noreply.github.com>
2021-12-24 13:05:07 -0300

    Delete Trabalho directory

--- functions.c
+++ functions.c
@@ -1,20 +1,20 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-//verifica se o elemento em questão está repetido no conjunto, retorna 0 se NÃO, 1 se SIM.
-bool verifica_repeticao(int elemento, int *conjunto, int num_elementos) {
+//verifica se existe um elemento um array aquele elemento
+bool array_elemento(int elemento, int conjunto[], int tam_array) {
     //armazena se há elementos repetidos no conjunto
     bool repetido;
 
     /** EXCLUIR NO FINAL ESSE COMENTARIO
      * @param elemento elemento que verá se está repetido no conjunto
      * @param conjunto array com os elementos do conjunto
-     * @param num_elementos numero de elementos a serem verificados a partir do elemento de indice 0. Ex.: se num_elementos = 2, então verifica indices 0 e 1;
+     * @param tam_array tamanho do array que será verificado. Ex.: se tam_array = 2, então verifica indices 0, 1 e 2;
     */
 
     //verifica se há elementos repetidos no conjunto
 
-    for (int i = 0; i < num_elementos; i++) {
+    for (int i = 0; i <= tam_array; i++) {
         if (conjunto[i] == elemento) {
             repetido = true;
             break;
