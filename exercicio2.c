df02714af612d7d37b83f8b2e0133edbae0b2846
anewere <umarthurqualquer74@gmail.com>
2021-12-24 14:05:31 -0300

    confirmar

--- exercicio2.c
+++ exercicio2.c
@@ -7,17 +7,36 @@
 #include <stdbool.h>
 
 int main() {
-
     srand(time(NULL));
     int i, j;
+    int conjunto_a[tam_a], conjunto_b[tam_b];
 
-    int conjunto_a[20]; //, conjunto_b[5];
-
-    //percorre o conjunto_a
+    //preenche o conjunto_a com numeros para que tenha cardinalidade 20
     for (i = 0; i < tam_a; i++) {
+        int elemento_de_a = rand() % 50;
+        //verifica se o elemento já está no conjunto e o recria se ja existir
+        if (array_elemento(elemento_de_a, conjunto_a, tam_a)) {
+            --i;
+        } else {
+            conjunto_a[i] = elemento_de_a;
+        }
     }
+    //exibe conjunto_a
+    array_exibe(conjunto_a, tam_a);
 
-    printf("%d", array_elemento(7, conjunto_a, 3));
+    //preenche o conjunto_b com numeros para que tenha cardinalidade 5
+    for (i = 0; i < tam_b; i++) {
+        int elemento_de_b = rand() % 50;
+        //verifica se o elemento já está no conjunto e o recria se ja existir
+        if (array_elemento(elemento_de_b, conjunto_b, tam_b)) {
+            --i;
+        } else {
+            conjunto_b[i] = elemento_de_b;
+        }
+    }
+    //exibe conjunto_b
+    printf("\n");
+    array_exibe(conjunto_b, tam_b);
 
     return 0;
 }
--- functions.c
+++ functions.c
@@ -3,14 +3,13 @@
 
 //verifica se existe um elemento um array aquele elemento
 bool array_elemento(int elemento, int conjunto[], int tam_array) {
-    //armazena se há elementos repetidos no conjunto
-    bool repetido;
-
     /** EXCLUIR NO FINAL ESSE COMENTARIO
      * @param elemento elemento que verá se está repetido no conjunto
      * @param conjunto array com os elementos do conjunto
      * @param tam_array tamanho do array que será verificado. Ex.: se tam_array = 2, então verifica indices 0, 1 e 2;
     */
+    //armazena se há elementos repetidos no conjunto
+    bool repetido;
 
     //verifica se há elementos repetidos no conjunto
 
@@ -23,4 +22,15 @@ bool array_elemento(int elemento, int conjunto[], int tam_array) {
         }
     }
     return repetido;
+}
+
+//exibe um array
+void array_exibe(int array[], int tam_array) {
+    /** EXCLUIR NO FINAL ESSE COMENTARIO
+     * @param array o array que erá recebido
+     * @param tam_array tamanho do array que será verificado. Ex.: se tam_array = 2  {?, ?}
+    */
+    for (int i = 0; i < tam_array; i++) {
+        printf("%d ", array[i]);
+    }
 }
